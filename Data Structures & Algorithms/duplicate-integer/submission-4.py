48f8711994098250c260f5cf865bfe02e11b6100
Olamide Treasure Oluwalade <112403968+treasureola@users.noreply.github.com>
2026-09-07 17:56:03 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash = set()
+        for n in nums:
+            if n in hash:
+                return True
+            else:
+                hash.add(n) 
+        return False
+            
+        
+         
